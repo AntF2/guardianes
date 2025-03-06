5a1f1f4eb88d7700f979b425ed26e34482a60a68
AntF2 <lomasbueno5000@gmail.com>
2025-03-06 11:10:09 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PrivateComponent } from './pages/private/private.component';
+import { loginGuard } from './guards/login.guard';
+import { ContactosComponent } from './pages/contactos/contactos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "private",
+    component: PrivateComponent, canActivate: [loginGuard]
+  },
+  {
+    path: "contactos",
+    component: ContactosComponent
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
---
+++ src/app/components/formulario-login/formulario-login.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+
+@Component({
+  selector: 'app-formulario-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './formulario-login.component.html',
+  styleUrl: './formulario-login.component.css'
+})
+export class FormularioLoginComponent {
+
+  constructor (private sercivio: LoginService) {}
+
+
+
+  email: any;
+  password: any;
+
+  login(loginForm: any) {
+    //  console.log(loginForm.value);
+    this.sercivio.postLogin(loginForm.value).subscribe(acceso=>{
+      //console.log(acceso);
+      let token = acceso.accesstoken
+      if(token! = ''){
+      localStorage.setItem("login", "true");
+      }
+    });
+  }
+
+  /*ngOnInit() {
+    this.sercivio.getLogin().subscribe()
+  }*/
+
+
+}
